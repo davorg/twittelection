db99afb31e4d59f4188b105399c2350ef4b772ce
Dave Cross <dave.cross@net-a-porter.com>
2015-01-14 12:13:15 +0000

    Created a get_schema() method (tired of writing the same code again and again)

--- lib/TwittElection/Schema.pm
+++ lib/TwittElection/Schema.pm
@@ -16,5 +16,26 @@ __PACKAGE__->load_namespaces;
 
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
+
+sub get_schema {
+  my $class = shift;
+
+  my @vars = qw(TE_HOST TE_DB TE_USER TE_PASS);
+  my @missing;
+
+  foreach (@vars) {
+    push @missing, $_ unless defined $ENV{$_};
+  }
+
+  if (@missing) {
+    die "You need to define this environment variables: @missing\n";
+  }
+
+  return $class->connect(
+    "dbi:mysql:$ENV{TE_DB}", $ENV{TE_USER}, $ENV{TE_PASS},
+    { mysql_enable_utf8 => 1 },  
+  ) or die;
+}
+
 __PACKAGE__->meta->make_immutable(inline_constructor => 0);
 1;
