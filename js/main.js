85a72084851eb105fbc7b89c9f40422dfbfb182a
Dave Cross <dave@dave.org.uk>
2015-01-26 20:54:02 +0000

    Added scroll to internal links.

--- js/main.js
+++ js/main.js
@@ -9,4 +9,19 @@ function setCounter () {
   $('#counter').html( daysToGo() + " Days to go" );
 }
 
-$( document ).ready( setCounter );
+$( document ).ready(function() {
+  setCounter;
+
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+        window.location.hash = target;
+    });
+  });
+} );
