186640d278558e7e1c35d65e36b3a3c64d6bce90
Dave Cross <dave@dave.org.uk>
2015-05-13 21:07:04 +0100

    Countdown to 2020

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 function daysToGo () {
-  var targetDate = new Date("May 7, 2015 00:00:00");
+  var targetDate = new Date("May 7, 2020 00:00:00");
   var secsToGo = (targetDate - new Date()) / 1000;
 
   return Math.floor(secsToGo / (60 * 60 * 24)) + 1;
