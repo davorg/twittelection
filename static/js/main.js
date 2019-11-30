53f2af8b960ae127eb802c98c38dae604932fd08
Dave Cross <dave@dave.org.uk>
2019-11-30 10:54:15 +0000

    Add static directory containing... well... static bits of the site

---
+++ static/js/main.js
@@ -0,0 +1,32 @@
+function daysToGo () {
+  var targetDate = new Date("December 12, 2019 00:00:00");
+  var secsToGo = (targetDate - new Date()) / 1000;
+
+  return Math.floor(secsToGo / (60 * 60 * 24)) + 1;
+}
+
+function setCounter () {
+  $('#counter').html( daysToGo() + " Days to go" );
+}
+
+$( document ).ready(function() {
+  setCounter();
+
+  $(function(){
+    $('#tablesorter').tablesorter();
+  });
+/*
+  $('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+        window.location.hash = target;
+    });
+  });
+*/
+} );
